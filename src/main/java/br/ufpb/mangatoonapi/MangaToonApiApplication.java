95dcd1eec33bf03fb514c221549039a9ab71f97f
TioSam02 <w02.barcelos@gmail.com>
2024-09-30 19:31:34 -0300

    Iniciando o projeto

---
+++ src/main/java/br/ufpb/mangatoonapi/MangaToonApiApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.mangatoonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaToonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaToonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpb/mangatoonapi/MangaToonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.mangatoonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaToonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
